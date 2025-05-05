0ae622e745a9b66f4226a48f08ece123ff16a189
gonzaroman <gonzaloromanmarquez@gmail.com>
2025-05-05 11:36:36 +0200

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { JuegoComponent } from "./components/juego/juego.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JuegoComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
